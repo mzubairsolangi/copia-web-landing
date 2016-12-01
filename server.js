cfe7b1d0355baa066ca2924f6579131abd7d2b22
Muhammad Zubair <muhammad.zubair@mobilelive.ca>
2016-12-01 16:00:56 +0500

    remove comments from server.js file

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.set('views', path.join(__dirname, 'src'));
 app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
