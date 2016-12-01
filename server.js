bb7d5c12258e738d4959600af0280a545ea13b5c
Muhammad Zubair <muhammad.zubair@mobilelive.ca>
2016-12-01 15:21:50 +0500

    Created header for home page.

--- server.js
+++ server.js
@@ -33,6 +33,12 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 /// error handlers
 
 // development error handler
