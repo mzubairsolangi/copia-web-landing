a38991d0effda7593f08f3f1791566cec0a7b741
Muhammad Zubair <muhammad.zubair@mobilelive.ca>
2016-12-03 19:55:40 +0500

    update serverjs file

--- server.js
+++ server.js
@@ -63,13 +63,24 @@ app.use(function(err, req, res, next) {
   });
 });
 
-
+var config = {
+  production: {
+    baseUrl: "https://copia.herokuapp.com/",
+    mongodbURL: "mongodb://schoolsystem:schoolsystem@ds015750.mlab.com:15750/schoolsystem"
+  },
+  development: {
+    baseUrl: "http://localhost:3000/",
+    //mongodbURL: "mongodb://localhost/scrubs"
+    mongodbURL: "mongodb://localhost/schoolManagementSystem"
+    //mongodbURL: "mongodb://scrubs:scrubs@ds061621.mongolab.com:61621/scrubs"
+  }
+};
 
 process.env.NODE_ENV = process.env.NODE_ENV || "production";
-//process.env.NODE_ENV = process.env.NODE_ENV || "development";
+ //process.env.NODE_ENV = process.env.NODE_ENV || "development";
 console.log("Node Environment = " + process.env.NODE_ENV);
 
-//app.config = config[process.env.NODE_ENV];
+app.config = config[process.env.NODE_ENV];
 
 //app.listen(3000, function () {
 app.listen(process.env.NODE_ENV || 3000, function () {
